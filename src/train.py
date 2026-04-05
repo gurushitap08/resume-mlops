47cd3421bf37cacd593737212bb8e272fc045c87
Guru Shitap <shitapguru921@gmail.com>
2026-04-05 14:37:33 +0530

    removed mlflow from pipeline

--- src/train.py
+++ src/train.py
@@ -1,6 +1,4 @@
 import pandas as pd
-import mlflow
-import mlflow.sklearn
 import re
 import os
 
@@ -9,76 +7,49 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 
-# =========================
-# STEP 1: Create folders
-# =========================
+# Create model folder
 os.makedirs("models", exist_ok=True)
 
-# =========================
-# STEP 2: Load dataset
-# =========================
+# Load dataset
 df = pd.read_csv("data/resume.csv")
 
 print("Dataset loaded successfully")
 print("Columns:", df.columns)
 
-# =========================
-# STEP 3: Clean text
-# =========================
+# Clean text
 def clean_text(text):
     text = str(text)
-    text = re.sub(r'<.*?>', '', text)   # remove HTML tags
-    text = re.sub(r'[^a-zA-Z ]', '', text)  # remove special chars
-    text = text.lower()
-    return text
+    text = re.sub(r'<.*?>', '', text)
+    text = re.sub(r'[^a-zA-Z ]', '', text)
+    return text.lower()
 
 df["Resume_str"] = df["Resume_str"].apply(clean_text)
 
-# =========================
-# STEP 4: Define features
-# =========================
+# Features
 X = df["Resume_str"]
 y = df["Category"]
 
-# =========================
-# STEP 5: Train-test split
-# =========================
+# Split
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
 )
 
-# =========================
-# STEP 6: Convert text → numbers
-# =========================
+# Vectorize
 vectorizer = TfidfVectorizer(stop_words="english")
 X_train_vec = vectorizer.fit_transform(X_train)
 X_test_vec = vectorizer.transform(X_test)
 
-# =========================
-# STEP 7: Model
-# =========================
+# Model
 model = LogisticRegression(max_iter=200)
-
-# =========================
-# STEP 8: MLflow tracking
-# =========================
-import os
-
-USE_MLFLOW = os.getenv("USE_MLFLOW", "1") == "1"
-
-if USE_MLFLOW:
-    mlflow.start_run()
 
 # Train
 model.fit(X_train_vec, y_train)
 
+# Predict
 y_pred = model.predict(X_test_vec)
-acc = accuracy_score(y_test, y_pred)
 
-if USE_MLFLOW:
-    mlflow.log_metric("accuracy", acc)
-    mlflow.sklearn.log_model(model, "model")
-    mlflow.end_run()
+# Accuracy
+acc = accuracy_score(y_test, y_pred)
 
 print("================================")
 print("Model trained successfully")
